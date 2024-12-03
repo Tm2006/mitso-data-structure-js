ec61ba5bb9f39e761db437b0da16ea8acaedd336
Tima <timon.mihalev@gmail.com>
2024-12-03 22:48:24 +0300

    feat: queue completed

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -13,21 +13,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-module.exports = class Queue {
+class ListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+module.exports = class Queue {
+  constructor() {
+    this.head = null; // Points to the front of the queue
+    this.tail = null; // Points to the end of the queue
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head; // Return the linked list starting from the head
+  }
+
+  enqueue(value) {
+    const newNode = new ListNode(value);
+    if (!this.tail) {
+      // If the queue is empty, both head and tail point to the new node
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // Add the new node to the end of the queue and update the tail
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) {
+      // If the queue is empty, return null
+      return null;
+    }
+    const dequeuedValue = this.head.value; // Get the value of the head node
+    this.head = this.head.next; // Move the head to the next node
+    if (!this.head) {
+      // If the queue becomes empty, update the tail to null
+      this.tail = null;
+    }
+    return dequeuedValue;
   }
+};
 
-}
